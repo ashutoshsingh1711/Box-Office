76b7181248bfc6b34bd3e095ae96d600e928f2f2
ashutoshsingh1711 <ashutoshsingh1711@gmail.com>
2021-01-21 01:28:25 +0530

    Updates

---
+++ precache-manifest.63984fd028527e3e85b2cab7c5642661.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9851317e190a58bf19619992f862b2b3",
+    "url": "/Box-Office/index.html"
+  },
+  {
+    "revision": "b9eb73ce33ffd31733dd",
+    "url": "/Box-Office/static/css/main.93052401.chunk.css"
+  },
+  {
+    "revision": "b5babcfc8a3fa6834439",
+    "url": "/Box-Office/static/js/2.0bd5ea4a.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/Box-Office/static/js/2.0bd5ea4a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b9eb73ce33ffd31733dd",
+    "url": "/Box-Office/static/js/main.aafb78ad.chunk.js"
+  },
+  {
+    "revision": "a0191adcd67e6a18cfb3",
+    "url": "/Box-Office/static/js/runtime-main.878dcaae.js"
+  }
+]);
